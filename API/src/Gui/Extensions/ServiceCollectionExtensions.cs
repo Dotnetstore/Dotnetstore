b985db65b21a10dedb93512edec51de165d21cc3
Hans Sjödin <hasse29@hotmail.com>
2024-10-03 11:13:41 +0200

    Added Dependency Injection

---
+++ API/src/Gui/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using SharedKernel.Extensions;
+
+namespace Gui.Extensions;
+
+internal static class ServiceCollectionExtensions
+{
+    internal static IServiceCollection AddGui(this IServiceCollection services)
+    {
+        services
+            .AddSharedKernel();
+
+        return services;
+    }
+}
---
+++ API/src/SharedKernel/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SharedKernel.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSharedKernel(this IServiceCollection services)
+    {
+        services
+            .AddFastEndpoints();
+
+        return services;
+    }
+}
